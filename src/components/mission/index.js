dcd2800d75bbefef802216d3cad1f3f60117c1b7
Patternscompany <pandureddypatterns@gmail.com>
2025-12-01 10:42:28 +0530

    pp

--- src/components/mission/index.js
+++ src/components/mission/index.js
@@ -1,60 +1,114 @@
-import React from 'react'
-import ms1 from '../../images/mission/icon1.png'
-import ms2 from '../../images/mission/icon2.png'
-import ms3 from '../../images/mission/icon3.png'
-import ms4 from '../../images/mission/icon4.png'
-import ms5 from '../../images/about6.png'
+import React from "react";
+import Slider from "react-slick";
 
-import './style.css'
+import img1 from "../../images/about6.png";
+import img2 from "../../images/about4.png";
+import img3 from "../../images/about5.png";
+
+import { Link } from "react-router-dom";
+import "./style.css";
 
 const Mission = (props) => {
-    return (
-        <div className={`wpo-mission-area ${props.subclass}`}>
-            <div className="container">
+  // Slick Settings
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 600,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  };
 
-
-                <div className="row">
-                    <div className="col-12">
-                        <div className="wpo-section-title">
-                            <span style={{fontSize:'16px', fontWeight:'500', color:'#91278f'}}>OUR PARTNERING ORGS</span>
-                            <h2 style={{fontSize:'36px', fontWeight:'600', lineHeight:'44px',letterSpacing:'2%'}}>Partners That Trusted Us to Amplify Impacts</h2>
-                            <p style={{color:'#37323C'}}>We have strategic partners, leaders from the private, public, and non-governmental organization (NGO) sectors, and subject matter experts in a variety of areas within children’s programs internationally.  </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="wpo-mission-wrap">
-                    <div className="row align-items-center">
-                        <div className='col-lg-6'>
-                            <div className="wpo-mission-content">
-                                <h2>Children Guidance center</h2>
-                                <h2>Community and rural development society</h2>
-                                <h2>Aasha jyothi Welfare</h2>
-                                <h2>Pavani special school</h2>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 col-md-6 col-sm-12 col-12 custom-grid">
-                            <div className="wpo-mission-item">
-                                <div className="wpo-mission-icon-5">
-                                    <img src={ms5} alt="" />
-                                </div>
-
-                            </div>
-                        </div>
-
-
-                        <div className="col-lg-12 col-md-12 col-sm-12 col-12 custom-grid">
-                            <div className="wpo-mission-item">
-
-                                <div className="wpo-mission-content">
-                                    <p>The Child Guidance Center encompasses 14 boarding schools* for children with disabilities, providing lodging and meals as well as medical care and education appropriate to the ability of each student. A 15th boarding school is currently under construction.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className={`wpo-mission-area ${props.subclass}`}>
+      <div className="container">
+        {/* Section Title */}
+        <div className="row">
+          <div className="col-12">
+            <div className="wpo-section-title">
+              <span
+                style={{
+                  fontSize: "16px",
+                  fontWeight: "500",
+                  color: "#91278f",
+                }}
+              >
+                OUR PARTNERING ORGS
+              </span>
+              <h2
+                style={{
+                  fontSize: "36px",
+                  fontWeight: "600",
+                  lineHeight: "44px",
+                  letterSpacing: "2%",
+                }}
+              >
+                Partners That Trusted Us to Amplify Impacts
+              </h2>
+              <p style={{ color: "#37323C" }}>
+                We have strategic partners, leaders from the private, public,
+                and NGO sectors, supporting children’s programs internationally.
+              </p>
             </div>
+          </div>
         </div>
-    )
-}
+
+        {/* Main Content */}
+        <div className="wpo-mission-wrap">
+          <div className="row align-items-center">
+            {/* LEFT SIDE - Partner Links */}
+            <div className="col-lg-6">
+              <div className="partner-links">
+                <h3>
+                  <Link to="/childcenter">Children Guidance Center</Link>
+                </h3>
+                <h3>
+                  <Link to="/cards">
+                    Community & Rural Development Society (CARDS)
+                  </Link>
+                </h3>
+                <h3>
+                  <Link to="/asha">Aasha Jyothi Welfare</Link>
+                </h3>
+                <h3>
+                  <Link to="/pavani">Pavani Special School</Link>
+                </h3>
+              </div>
+            </div>
+
+            {/* RIGHT SIDE - Image Carousel */}
+            <div className="col-lg-6">
+              <Slider {...settings}>
+                <div>
+                  <img src={img1} className="carousel-img w-100" alt="" />
+                </div>
+                <div>
+                  <img src={img2} className="carousel-img w-100" alt="" />
+                </div>
+                <div>
+                  <img src={img3} className="carousel-img w-100" alt="" />
+                </div>
+              </Slider>
+            </div>
+
+            {/* Bottom Description */}
+            <div className="col-12">
+              <div className="wpo-mission-item">
+                <p>
+                  The Child Guidance Center operates 14 boarding schools for
+                  children with disabilities, offering housing, education, and
+                  medical support. A 15th school is currently under
+                  construction.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Mission;
