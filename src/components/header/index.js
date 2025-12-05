d90c449cc7e67fd4c27c52622ae19e79818ae4b5
Patternscompany <pandureddypatterns@gmail.com>
2025-12-05 11:24:42 +0530

    pp

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,95 +1,154 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import HeaderTopbar from '../HeaderTopbar'
 import MobileMenu from '../../components/MobileMenu'
-import min1 from '../../images/shop/mini-cart/img-1.jpg'
-import min2 from '../../images/shop/mini-cart/img-2.jpg'
 import './style.css'
 
 const Header = () => {
-    const SubmitHandler = (e) => {
-        e.preventDefault()
-    }
-    
 
-    return (
-        <div className="middle-header header-style-3">
-            <HeaderTopbar />
-            <div className="container">
-                <div className="header-content">
-                    <div className="row">
+  useEffect(() => {
+    // select all sub-menu parent dropdowns
+    const dropdownParents = document.querySelectorAll("li:has(ul)");
 
-                        <div className="col-lg-12 d-lg-block d-none">
+    dropdownParents.forEach(parent => {
+      const submenuLinks = parent.querySelectorAll("ul li a");
 
+      submenuLinks.forEach(link => {
+        link.addEventListener("click", () => {
+          // add class that hides dropdown
+          parent.classList.add("closed");
 
+          // remove class after short delay so hover works again
+          setTimeout(() => {
+            parent.classList.remove("closed");
+          }, 300);
+        });
+      });
+    });
+  }, []);
 
-                            <nav className='row'>
-                                <ul className="col-lg-9">
-                                    <li><Link className="" to="/about" title="">About Us</Link>
+  return (
+    <div className="middle-header header-style-3">
+        <style>{`
+        /* make li the positioning parent */
+        .middle-header.header-style-3 nav ul li {
+          position: relative;
+        }
 
-                                    </li>
-                                    <li><Link to="/" className='drop-downn' title="">Our Programs</Link>
-                                        <ul>
-                                            <li><Link to="/disability" title="">Supporting Disability</Link></li>
-                                            <li><Link to="/education" title="">Education</Link></li>
-                                            <li><Link to="/medical" title="">Medical Support</Link></li>
+        /* hide all submenus by default */
+        .middle-header.header-style-3 nav ul li > ul {
+          position: absolute;
+          left: 0;
+          top: 100%;
+          min-width: 220px;
+          background: #ffffff;
+          padding: 10px 0;
+          margin: 0;
+          list-style: none;
+          opacity: 0;
+          visibility: hidden;
+          transform: translateY(10px);
+          transition: all 0.25s ease;
+          z-index: 999;
+        }
 
-                                            <li><Link to="/childtraffic" title="">Child trafficking</Link></li>
-                                            <li><Link to="/tripofindia" title="">Trip to India</Link></li>
-                                            <li><Link to="/nurturingplaytime" title="">Before/ After school program</Link></li>
-                                            <li><Link to="/disasterrelief" title="">Disaster emergency relief</Link></li>
-                                        </ul>
-                                    </li>
-                                    <li><Link to="/ourpartners" className='drop-downn' title="">Our Partners</Link>
-                                        <ul>
-                                            <li><Link to="/childcenter" title="">Center GC</Link></li>
-                                            <li><Link to="/pavani" title="">Pavani</Link></li>
-                                            <li><Link to="/cards" title="">CARDS</Link></li>
-                                            <li><Link to="/asha" title="">Asha jyothi</Link></li>
-                                            {/* <li><Link to="/case-single" title="">TrueBlue</Link></li>
-                                            <li><Link to="/case-single" title="">Emmi Group</Link></li>  */}
+        .middle-header.header-style-3 nav ul li > ul li a {
+          display: block;
+          padding: 6px 20px;
+          white-space: nowrap;
+        }
 
-                                        </ul>
-                                    </li>
-                                    {/* <li><Link to="/" title="">Our Impacts</Link>
+        /* show submenu on HOVER */
+        .middle-header.header-style-3 nav ul li:hover > ul {
+          opacity: 1;
+          visibility: visible;
+          transform: translateY(0);
+        }
+          /* Hide dropdown by default */
+nav ul li ul {
+  display: none;
+  opacity: 0;
+  visibility: hidden;
+  transition: 0.3s ease;
+  position: absolute;
+}
 
-                                    </li> */}
-                                    <li><Link to="/stayinformed" title="">Stay Informed</Link>
+/* Show dropdown on hover */
+nav ul li:hover > ul {
+  display: block;
+  opacity: 1;
+  visibility: visible;
+}
 
-                                    </li>
-                                      <li><Link to="/getinvolved" title="">Get Involved</Link>
+/* Close dropdown when the parent has .closed class */
+nav ul li.closed > ul {
+  display: none !important;
+  opacity: 0 !important;
+  visibility: hidden !important;
+}
 
-                                    </li>
+      `}</style>
+      <HeaderTopbar />
+      <div className="container">
+        <div className="header-content">
+          <div className="row">
 
-                                    {/* <li><Link to="/contact" title="">How to be involved?</Link></li> */}
-                                </ul>
-                                <ul className="col-lg-3">
-                                    <li><Link to="/resources" className='' title="">Resources</Link>
-                                    </li>
-                                    <li><Link to="/faq" title="">F.A.Q</Link></li>
-                                    <li><Link to="/" title="">Subscribe</Link></li>
+            <div className="col-lg-12 d-lg-block d-none">
 
+              <nav className='row'>
+                <ul className="col-lg-9">
 
+                  <li><Link to="/about">About Us</Link></li>
 
-                                </ul>
-                            </nav>
+                  <li>
+                    <Link to="/" className='drop-downn'>Our Programs</Link>
+                    <ul>
+                      <li><Link to="/disability">Supporting Disability</Link></li>
+                      <li><Link to="/education">Education</Link></li>
+                      <li><Link to="/medical">Medical Support</Link></li>
+                      <li><Link to="/childtraffic">Child Trafficking</Link></li>
+                      <li><Link to="/tripofindia">Trip to India</Link></li>
+                      <li><Link to="/nurturingplaytime">Before/After School</Link></li>
+                      <li><Link to="/disasterrelief">Disaster Relief</Link></li>
+                    </ul>
+                  </li>
 
+                  <li>
+                    <Link to="/ourpartners" className='drop-downn'>Our Partners</Link>
+                    <ul>
+                      <li><Link to="/childcenter">Center GC</Link></li>
+                      <li><Link to="/pavani">Pavani</Link></li>
+                      <li><Link to="/cards">CARDS</Link></li>
+                      <li><Link to="/asha">Asha Jyothi</Link></li>
+                    </ul>
+                  </li>
 
+                  <li><Link to="/stayinformed">Stay Informed</Link></li>
+                  <li><Link to="/getinvolved">Get Involved</Link></li>
 
-                        </div>
-                        <div className="col-md-2 col-sm-2 col-2">
-                            <MobileMenu />
-                        </div>
-                    </div>
+                </ul>
 
+                <ul className="col-lg-3">
+                  <li><Link to="/resources">Resources</Link></li>
+                  <li><Link to="/faq">F.A.Q</Link></li>
+                  <li><Link to="/">Subscribe</Link></li>
+                </ul>
 
-                </div>
+              </nav>
 
-                <div className="clearfix"></div>
             </div>
 
+            <div className="col-md-2 col-sm-2 col-2">
+              <MobileMenu />
+            </div>
+
+          </div>
         </div>
-    )
-}
+
+        <div className="clearfix"></div>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
